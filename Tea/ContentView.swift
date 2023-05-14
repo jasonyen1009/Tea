ec670c1d7595f88d68b2f9323a02023efd7cf8d7
Jason <jasonyen1009@gmail.com>
2023-05-14 13:49:49 +0800

    Initial Commit

---
+++ Tea/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Tea
+//
+//  Created by Yen Hung Cheng on 2023/5/14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Tea/TeaApp.swift
@@ -0,0 +1,17 @@
+//
+//  TeaApp.swift
+//  Tea
+//
+//  Created by Yen Hung Cheng on 2023/5/14.
+//
+
+import SwiftUI
+
+@main
+struct TeaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
